4cdc7b3448f97ebdf3fee8a9ec357e654fa9e7b2
DavidBert <davidbertoin@gmail.com>
2021-10-22 17:18:24 +0200

    remove pin memory

--- data_utils.py
+++ data_utils.py
@@ -10,7 +10,7 @@ def get_cifar10_train_loader():
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    return torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=8, pin_memory=True)
+    return torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=8)
 
 def get_cifar10_test_loader():
     transform = transforms.Compose([
@@ -18,7 +18,7 @@ def get_cifar10_test_loader():
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
     testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-    return torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8, pin_memory=True)
+    return torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8)
 
 def get_cifar10_loaders():
     trainloader = get_cifar10_train_loader()
@@ -32,8 +32,8 @@ def get_svhn_loaders():
     
     trainset = torchvision.datasets.SVHN(root='./data', split='extra', download=True, transform=transform)
     testset = torchvision.datasets.SVHN(root='./data', split='test', download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=8, pin_memory=True)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8, pin_memory=True)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=8)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8)
     return trainloader, testloader
 
 def get_mnist_loaders(**kwargs):
@@ -45,6 +45,6 @@ def get_mnist_loaders(**kwargs):
 
     trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
     testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=4, pin_memory=True)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=4, pin_memory=True)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=4)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=4)
     return trainloader, testloader
